cd9deaccf19f7d360d98371d1ee6d26b5fbb8e99
Abhishek Kumar Bauri <abhishekbauri12@gmail.com>
2023-12-14 20:42:36 +0530

    Updating current user data

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,18 @@
+const AppError = require('../utils/appError');
 const catchAsync = require('../utils/catchAsync');
 const User = require('./../models/userModel');
 const APIFeatures = require('./../utils/apiFeatures');
+
+const filterObj = (obj, ...allowedFields) => {
+  const newObj = {};
+  // Object.keys(obj) will return an array containing all the fields present in the obj
+  Object.keys(obj).forEach((el) => {
+    if (allowedFields.includes(el)) {
+      newObj[el] = obj[el];
+    }
+  });
+  return newObj;
+};
 
 // user route handler
 exports.getAllUsers = catchAsync(async (req, res, next) => {
@@ -12,6 +24,35 @@ exports.getAllUsers = catchAsync(async (req, res, next) => {
     results: users.length,
     data: {
       users,
+    },
+  });
+});
+
+exports.updateMe = catchAsync(async (req, res, next) => {
+  // 1) Create error if user POSTs password data
+  if (req.body.password || req.body.passwordConfirm) {
+    return next(
+      new AppError(
+        'This route is not for password updates. Please use /updatePassword',
+        400,
+      ),
+    );
+  }
+
+  // 2) Filtered out the field name that are not allowed to update
+  const filteredBody = filterObj(req.body, 'name', 'email');
+
+  // 3) Update user document
+  // here using findById and then saving the data will give error because passwordConfirm field is required.
+  const updatedUser = await User.findByIdAndUpdate(req.user.id, filteredBody, {
+    new: true,
+    runValidators: true,
+  });
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      user: updatedUser,
     },
   });
 });
